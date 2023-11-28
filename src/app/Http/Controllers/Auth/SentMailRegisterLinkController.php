231079d107c6343c579f03f2ae28555c20317b47
Andrew Filkin <andrewfilkin@MacBook-Air-Andrew.local>
2023-11-28 22:29:15 +0200

    Possibility Send email using Queue (database)

--- src/app/Http/Controllers/Auth/SentMailRegisterLinkController.php
+++ src/app/Http/Controllers/Auth/SentMailRegisterLinkController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\SendRegisterLinkMail;
+use App\Jobs\SendConfirmRegisterLinkToEmailJob;
 
 class SentMailRegisterLinkController extends Controller
 {
     public function send()
     {
-        $link = 'https://google.com';
-        Mail::to('recipient@example.com')->send(new SendRegisterLinkMail($link));
+        dispatch(new SendConfirmRegisterLinkToEmailJob());
     }
+
 }
---
+++ src/app/Jobs/SendConfirmRegisterLinkToEmailJob.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\SendRegisterLinkMail;
+
+class SendConfirmRegisterLinkToEmailJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        sleep(15);
+        $link = 'https://google.com';
+        Mail::to('recipient@example.com')->send(new SendRegisterLinkMail($link));
+    }
+}
